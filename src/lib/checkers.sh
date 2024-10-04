66aef14fbdc92e6c367caf241c3e96fb4fa388d4
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2024-10-03 23:41:58 -0600

    refacor: move variables and function used one time
    Move one-time-use variables and functions used only for ls command to
    the proper file.
    
    Add 'simple_software' array easy installation of this kind of programs.

--- src/lib/checkers.sh
+++ src/lib/checkers.sh
@@ -1,35 +1,20 @@
-## Add any function here that is needed in more than one parts of your
-## application, or that you otherwise wish to extract from the main function
-## scripts.
-##
-## Note that code here should be wrapped inside bash functions, and it is
-## recommended to have a separate file for each function.
-##
-## Subdirectories will also be scanned for *.sh, so you have no reason not
-## to organize your code neatly.
-##
+declare -a simple_software
 
-declare -A software_status
-declare -A software_checker
-
-software_checker=(
-  [git]="check_dnf_package"
-  [fzf]="check_dnf_package"
-  [eza]="check_dnf_package"
-  [git-delta]="check_dnf_package"
-  [bat]="check_dnf_package"
-  [bfs]="check_dnf_package"
-  [neovim]="check_dnf_package"
-  [yakuake]="check_dnf_package"
-  [lazygit]="check_dnf_package"
-  [code]="check_dnf_package"
-  [docker-ce-cli]="check_dnf_package"
-  [zoxide]="check_file ${HOME}/.local/bin/zoxide"
-  [jetbrains-toolbox]="check_file ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
+# Programs that only need `sudo dnf -y install <program>`
+# in order to be installed
+simple_software=(
+  git
+  fzf
+  eza
+  git-delta
+  bat
+  bfs
+  neovim
+  yakuake
 )
 
 check_dnf_package() {
-  if dnf list installed | grep -wq "$1"; then
+  if dnf -C list --installed "$1" 2> /dev/null > /dev/null; then
     echo true
   else
     echo false
@@ -42,16 +27,4 @@ check_file() {
   else
     echo false
   fi
-}
-
-check_software() {
-  for software in "${!software_checker[@]}"; do
-    local checker="${software_checker[$software]}"
-
-    if [[ "${checker}" != "check_dnf_package"* ]]; then
-      software_status["${software}"]=$(eval "${checker}")
-    else
-      software_status["${software}"]=$(eval "${checker}" "${software}")
-    fi
-  done
 }
--- src/list_command.sh
+++ src/list_command.sh
@@ -1,10 +1,3 @@
-## echo "# this file is located in 'src/list_command.sh'"
-## echo "# code for 'soft list' goes here"
-## echo "# you can edit it freely and regenerate (it will not be overwritten)"
-# inspect_args
-
-# "{{ ansible_env.HOME }}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
-
 is_installed() {
   if "$1"; then
     echo "✅"
@@ -13,8 +6,35 @@ is_installed() {
   fi
 }
 
-check_software
+declare -A software_checker
+declare -A software_status
 
+# Array that stores the way a program status is checked
+software_checker=(
+  [lazygit]="check_dnf_package"
+  [code]="check_dnf_package"
+  [docker-ce]="check_dnf_package"
+  [zoxide]="check_file ${HOME}/.local/bin/zoxide"
+  [jetbrains-toolbox]="check_file ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
+)
+
+# Add the 'simple software' to $software_checker
+for software in "${simple_software[@]}"; do
+  software_checker[$software]="check_dnf_package"
+done
+
+# Set the status of every program an store them in $software_status
+for software in "${!software_checker[@]}"; do
+  local checker="${software_checker[$software]}"
+
+  if [[ "${checker}" != "check_dnf_package" ]]; then
+    software_status["${software}"]=$(eval "${checker}")
+  else
+    software_status["${software}"]=$(eval "${checker}" "${software}")
+  fi
+done
+
+# Print a neatly table with programs and its statuses
 printf "┌───────────────────┬────┐\n"
 for software in "${!software_status[@]}"; do
   local status="${software_status[$software]}"
