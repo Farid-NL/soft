e027c0ffc77e9a729eed86d62c92deab9c5946b2
Farid NL <34426099+Farid-NL@users.noreply.github.com>
2024-10-04 00:14:38 -0600

    feat: add --status option to list command
    Refactor list_command.sh

--- src/list_command.sh
+++ src/list_command.sh
@@ -1,3 +1,41 @@
+status=${args[--status]}
+
+# Store the way a program status is checked
+declare -A software_checkers
+
+# Store the installation status of every program
+declare -A software_status
+
+
+populate_checkers() {
+  software_checkers=(
+    [lazygit]="check_dnf_package"
+    [code]="check_dnf_package"
+    [docker-ce]="check_dnf_package"
+    [zoxide]="check_file ${HOME}/.local/bin/zoxide"
+    [jetbrains-toolbox]="check_file ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
+  )
+
+  # Add the 'simple software' to $software_checkers
+  for software in "${simple_software[@]}"; do
+    software_checkers[$software]="check_dnf_package"
+  done
+}
+
+populate_status() {
+  populate_checkers
+
+  for software in "${!software_checkers[@]}"; do
+    local checker="${software_checkers[$software]}"
+
+    if [[ "${checker}" != "check_dnf_package" ]]; then
+      software_status["${software}"]=$(eval "${checker}")
+    else
+      software_status["${software}"]=$(eval "${checker}" "${software}")
+    fi
+  done
+}
+
 is_installed() {
   if "$1"; then
     echo "✅"
@@ -6,38 +44,20 @@ is_installed() {
   fi
 }
 
-declare -A software_checker
-declare -A software_status
+if [[ "${status}" ]]; then
+  populate_status
 
-# Array that stores the way a program status is checked
-software_checker=(
-  [lazygit]="check_dnf_package"
-  [code]="check_dnf_package"
-  [docker-ce]="check_dnf_package"
-  [zoxide]="check_file ${HOME}/.local/bin/zoxide"
-  [jetbrains-toolbox]="check_file ${HOME}/.local/share/JetBrains/Toolbox/bin/jetbrains-toolbox"
-)
+  printf "┌───────────────────┬────┐\n"
+  for software in "${!software_status[@]}"; do
+    local status="${software_status[$software]}"
+    printf "│ %17s │ %s │\n" "${software}" "$(is_installed ${status})"
+  done
+  printf "└───────────────────┴────┘\n"
 
-# Add the 'simple software' to $software_checker
-for software in "${simple_software[@]}"; do
-  software_checker[$software]="check_dnf_package"
-done
+else
+  populate_checkers
 
-# Set the status of every program an store them in $software_status
-for software in "${!software_checker[@]}"; do
-  local checker="${software_checker[$software]}"
-
-  if [[ "${checker}" != "check_dnf_package" ]]; then
-    software_status["${software}"]=$(eval "${checker}")
-  else
-    software_status["${software}"]=$(eval "${checker}" "${software}")
-  fi
-done
-
-# Print a neatly table with programs and its statuses
-printf "┌───────────────────┬────┐\n"
-for software in "${!software_status[@]}"; do
-  local status="${software_status[$software]}"
-  printf "│ %17s │ %s │\n" "${software}" "$(is_installed ${status})"
-done
-printf "└───────────────────┴────┘\n"
+  for software in "${!software_checkers[@]}"; do
+    echo "${software}"
+  done
+fi
